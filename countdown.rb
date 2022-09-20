13144c811703933469d1707f4e170d52bd676b8a
Tanner <troxs690@gmail.com>
2022-09-20 10:13:07 -0500

    commit tree 2959d8a0da5ccdea23ee2bc21a93e7762ac2d5c0

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown (int)
+int = 10
+  while int > 1
+  puts  "#{int} SECOND(S)!"
+    if int == 1
+   return   "HAPPY NEW YEAR!"
+    int -= 1
 end
